8a355318d3b5c8b0553305bad9720de99a192850
Houssem Eddine Chebbi <houssem.eddinechebbi.externe@darva.fr>
2021-08-25 20:46:41 +0200

    set up app.js & routes

--- Back-Node/app.js
+++ Back-Node/app.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const messagesRouter = require('./routes/messages');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
+// Middlewares
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/messages', require('./routes/messages'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.send(err);
+});
 
 module.exports = app;
